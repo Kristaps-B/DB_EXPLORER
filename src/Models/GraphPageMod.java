a12a3665bba86189f3b2ddc9425b19166fc3c394
Kristaps Babrovskis <kristaps.babrovskis_1@edu.rtu.lv>
2017-09-26 22:48:15 +0300

    Created  GRAPH joins

--- src/Models/GraphPageMod.java
+++ src/Models/GraphPageMod.java
@@ -3,7 +3,10 @@ package Models;
 import Database.SQLLite;
 import Global.Session;
 import Json.ArrayJson;
+import Results.JoinResult;
+import Results.TableJoinResult;
 import Results.TableResult;
+import Results.ViewJoinResult;
 
 public class GraphPageMod {
 	public GraphPageMod () {
@@ -23,9 +26,14 @@ public class GraphPageMod {
 		TableResult rs = new TableResult();
 		
 		
-		int id = 1;
+		
 		
 		String sql = "SELECT id, table_id, owner, table_name, examine_time FROM all_tables " ;
+		
+		
+		ArrayJson aJson = new ArrayJson ();
+		
+		
 		
 		try {
 			sqlLite.query(sql, rs, Session.dBUserString);
@@ -33,9 +41,8 @@ public class GraphPageMod {
 			
 			System.out.println("---------LOAD_TABLES--------------");
 			
-			
-			ArrayJson aJson = new ArrayJson ();
 			aJson.startJson();
+			
 			
 			for (int i = 0; i < rs.getColumns().size(); i++) {
 				
@@ -44,22 +51,22 @@ public class GraphPageMod {
 				// System.out.println(" " + row.table_name);
 				 
 				//  "color": "red", "size":40, "shape": "box"
-				aJson.addValue("id", "" + row.id);
+				aJson.addValue("id", "" + row.owner + "." +row.table_name);
 				aJson.addValue("label", row.owner + "." +row.table_name);
 				aJson.addValue("color", "green");
-				aJson.addValue("size", "60");
+				aJson.addValue("font", "20px arial white");
 				aJson.addValue("shape", "box");
 		
 				
 				aJson.newRow();
 				
-				id ++;
+	
 			}
 			
-			aJson.endJson();
 			
 			
-			result = "{ \"nodes\" : " + aJson.getJson() + " , \"edges\": " + "[ ]" + " }" ;
+			
+			
 			
 			
 		} catch (Exception e) {
@@ -67,6 +74,103 @@ public class GraphPageMod {
 			
 			
 		}
+		
+		
+		
+		
+		ArrayJson bJson = new ArrayJson ();
+		JoinResult rs1 = new JoinResult();
+		
+		
+		int id = 0;
+		
+		
+	 sql = "SELECT js.id, tl.table_name left_table_name, tr.table_name right_table_name, cl.column_name left_column_name, cr.column_name right_column_name, tl.owner left_owner, tr.owner right_owner " + 
+		" FROM all_table_joins j, all_tables tl, all_tables tr, all_columns cl, all_columns cr, join_sources js  " + 
+		" WHERE 1=1 " + 
+		" AND j.id                = js.join_id " +
+		" AND j.left_table_id     = tl.id " +
+		" AND j.right_table_id    = tr.id " +
+		" AND j.left_column_id    = cl.id " +
+		" AND j.right_column_id   = cr.id "
+		
+	   
+		;
+		
+		try {
+			
+			sqlLite.query(sql, rs1, Session.dBUserString);
+			
+			
+			System.out.println("--------- LOAD_JOINS --------------");
+			
+			
+			
+			bJson.startJson();
+			
+			for (int i = 0; i < rs1.getColumns().size(); i++) {
+				
+				 JoinResult.Row row = rs1.getColumns().get(i);
+				
+			
+				 
+				aJson.addValue("id", row.left_owner + "." + row.left_table_name + "." + row.left_column_name + "." + id);
+				aJson.addValue("label",  row.left_column_name);
+				
+				 
+				
+				aJson.newRow();
+				
+				
+				aJson.addValue("id", row.right_owner + "." + row.right_table_name + "." + row.right_column_name+ "." + id);
+				aJson.addValue("label",  row.right_column_name);
+				
+				
+				
+				aJson.newRow();
+				
+				
+				
+				// -- {"from": "1", "to": "3"} 
+				
+				bJson.addValue("from", row.left_owner + "." + row.left_table_name + "." + row.left_column_name+ "." + id);
+				bJson.addValue("to", row.right_owner + "." + row.right_table_name + "." + row.right_column_name+ "." + id);
+				
+				
+				bJson.newRow();
+				
+				
+				bJson.addValue("from", row.right_owner + "." +row.right_table_name);
+				bJson.addValue("to", row.right_owner + "." + row.right_table_name + "." + row.right_column_name+ "." + id);
+				
+				bJson.newRow();
+				
+				bJson.addValue("from", row.left_owner + "." +row.left_table_name);
+				bJson.addValue("to", row.left_owner + "." + row.left_table_name + "." + row.left_column_name+ "." + id);				
+				
+				
+				bJson.newRow();
+				
+				id ++;
+			}
+			
+		 
+			
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			
+			
+		}
+		
+		
+		aJson.endJson();
+		bJson.endJson();
+		
+		
+		result = "{ \"nodes\" : " + aJson.getJson() + " , \"edges\": "  + bJson.getJson() + " }" ;
 		
 		
 		
---
+++ src/Results/JoinResult.java
@@ -0,0 +1,70 @@
+package Results;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Results.UsersResult.Row;
+
+public class JoinResult extends Result {
+	private ArrayList <Row> columns = new ArrayList <Row> ();
+	
+	
+	public void copyRows(ResultSet rs) throws Exception {
+		
+		
+		
+		while (rs.next()) {
+			
+
+			
+			columns.add(new Row(
+					rs.getInt("ID"),
+					rs.getString  ("LEFT_TABLE_NAME"), 
+					rs.getString ("RIGHT_TABLE_NAME"),
+					rs.getString("LEFT_COLUMN_NAME"),
+					rs.getString("RIGHT_COLUMN_NAME"),
+					rs.getString("LEFT_OWNER"),
+					rs.getString("RIGHT_OWNER")
+					));			
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	public ArrayList <Row> getColumns () {
+		return columns;
+	}
+	
+	
+	
+	 public class Row {
+			
+			
+			public int id;
+			public String left_table_name;
+			public String right_table_name;	
+			public String left_column_name;
+			public String right_column_name;
+			public String left_owner;
+			public String right_owner;
+			
+			
+			public Row (int id, String left_table_name, String right_table_name, String left_column_name, String right_column_name, String left_owner, String right_owner) {
+				this.id = id;
+				this.left_table_name    = left_table_name;
+				this.right_table_name   = right_table_name;
+				this.left_column_name   = left_column_name;
+				this.right_column_name  = right_column_name;
+				this.left_owner         = left_owner;
+				this.right_owner        = right_owner;
+			}
+			
+
+			
+			
+		}
+}
